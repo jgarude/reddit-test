e73bca3f8e48ccb98880f8abb851da4ea3333d7c
Jitesh Garude <jitesh.garude@sapientrazorfish.com>
2021-01-13 16:26:24 +0530

    added reddit api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const [news, setNews] = useState([]);
+  const fetNews = async () => {
+    const res = await fetch("https://www.reddit.com/subreddits/popular.json");
+    const data = await res.json();
+    const newsData = data.data.children;
+    setNews(newsData);
+  };
+  useEffect(() => {
+    fetNews();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="news">
+      {news.map((item) => (
+        <div className="news-item">
+          <h3>
+            <img src={item.data.icon_img} alt="" /> {item.data.display_name}
+          </h3>
+          <img src={item.data.banner_img} alt="" />
+          <div
+            dangerouslySetInnerHTML={{ __html: item.data.public_description }}
+          />
+          <a href={`https://www.reddit.com${item.data.url}`} target="_blank">
+            Read More
+          </a>
+        </div>
+      ))}
     </div>
   );
 }
